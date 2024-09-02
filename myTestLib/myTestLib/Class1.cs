5e7a4a439b737399d3ac34131c0fd5f36f1a2e5d
PYIIIUCTUK <alexandrowitch.vlad2014@yandex.ru>
2024-09-02 17:57:19 +0300

    Initial commit

---
+++ myTestLib/myTestLib/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace myTestLib
+{
+	public interface IFigure
+	{
+		double CalculateArea();
+	}
+
+	public class Circle : IFigure
+	{
+		public double Radius { get; private set; }
+
+		public Circle(double radius)
+		{
+			if (radius <= 0) throw new ArgumentException($"Error: Raduis must be positive");
+
+			this.Radius = radius;	
+		}
+
+		public double CalculateArea()
+		{
+			return Math.Round(Math.PI * Math.Pow(Radius, 2), 1);
+		}
+	}
+
+	public class Triangle : IFigure
+	{
+		public double sideA { get; private set; }
+		public double sideB { get; private set; }
+		public double sideC { get; private set; }
+
+		public Triangle(double sideA, double sideB, double sideC)
+		{
+			if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+				throw new ArgumentException("Sides must be positive.");
+
+			if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+				throw new ArgumentException("There is no triangle with such sides.");
+
+			this.sideA = sideA;
+			this.sideB = sideB;
+			this.sideC = sideC;
+		}
+
+		public double CalculateArea()
+		{
+			double p = (sideA + sideB + sideC) / 2;
+			return Math.Round(Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC)), 1);
+		}
+
+		public bool isRectangular()
+		{
+			return	(sideA == Math.Sqrt(Math.Pow(sideB, 2) + Math.Pow(sideC, 2))
+					|| sideB == Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideC, 2))
+					|| sideC == Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2)));
+		}
+	}
+}
---
+++ myTestLib/myTestLibTests/Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using myTestLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myTestLib.Tests
+{
+	[TestClass()]
+	public class CircleTest
+	{
+		[TestMethod()]
+		public void Radius_5_Return_78_5()
+		{
+			// arrange
+			Circle circle = new Circle(5);
+			double expected = 78.5;
+
+			// act
+			double result = circle.CalculateArea();
+
+			// assert
+			Assert.AreEqual(expected, result);
+		}
+	}
+
+	[TestClass()]
+	public class TriangleTest
+	{
+		[TestMethod]
+		public void Square_5_5_6_Return_12()
+		{
+			// arrange
+			Triangle triangle = new Triangle(5, 5, 6);
+			double expected = 12;
+
+			// act
+			double result = triangle.CalculateArea();
+
+			// assert
+			Assert.AreEqual(expected, result);
+		}
+
+		[TestMethod]
+		public void Triangle_is_Rectangular_ReturnTrue()
+		{
+			// arrange
+			Triangle triangle = new Triangle(3, 4, 5);
+
+			// act
+			bool result = triangle.isRectangular();
+
+			// assert
+			Assert.IsTrue(result);
+		}
+	}
+}
